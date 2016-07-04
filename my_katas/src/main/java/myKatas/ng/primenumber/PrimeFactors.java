181ee41ffd493bf8222985a76a4638f634dfc233
mavericks065 <nicolas065@gmail.com>
2016-07-04 11:38:40 +1000

    add new kata

---
+++ my_katas/src/main/java/myKatas/ng/primenumber/PrimeFactors.java
@@ -0,0 +1,48 @@
+package myKatas.ng.primenumber;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Nicolas Guignard
+ */
+public class PrimeFactors {
+
+    public static List<Integer> generate(int number) {
+
+        return null;
+    }
+
+
+    /**
+     *
+     *
+     *
+     *
+     *
+     *
+     *                  SOLUTION
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     */
+
+
+//    public static List<Integer> generate(int number) {
+//
+//        final List<Integer> primeFactors = new ArrayList<>();
+//        for (int candidate = 2; number > 1; candidate++) {
+//            for (;number % candidate == 0; number /= candidate) {
+//                primeFactors.add(candidate);
+//            }
+//        }
+//
+//        return primeFactors;
+//    }
+}
---
+++ my_katas/src/test/java/myKatas/ng/primenumber/PrimeFactorsTest.java
@@ -0,0 +1,100 @@
+package myKatas.ng.primenumber;
+
+import com.google.common.collect.Lists;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Created by Nicolas Guignard
+ */
+public class PrimeFactorsTest {
+
+
+
+
+
+
+
+//    @Test
+//    public void should_test_one() throws Exception {
+//        // THEN
+//        Assert.assertEquals(Lists.newArrayList(), PrimeFactors.generate(1));
+//    }
+//
+//    @Test
+//    public void should_test_2() throws Exception {
+//        // GIVEN
+//        int n = 2;
+//
+//        // WHEN
+//        List<Integer> result = PrimeFactors.generate(n);
+//
+//        // THEN
+//        Assert.assertEquals(Lists.newArrayList(2), result);
+//
+//    }
+//
+//    @Test
+//    public void should_test_3() throws Exception {
+//        // GIVEN
+//        int n = 3;
+//
+//        // WHEN
+//        List<Integer> result = PrimeFactors.generate(n);
+//
+//        // THEN
+//        Assert.assertEquals(Lists.newArrayList(3), result);
+//    }
+//
+//    @Test
+//    public void should_test_four() throws Exception {
+//        // GIVEN
+//        int n = 4;
+//
+//        // WHEN
+//        List<Integer> result = PrimeFactors.generate(n);
+//
+//        // THEN
+//        Assert.assertEquals(Lists.newArrayList(2, 2), result);
+//    }
+//
+//    @Test
+//    public void should_test_six() throws Exception {
+//        // GIVEN
+//        int n = 6;
+//
+//        // WHEN
+//        List<Integer> result = PrimeFactors.generate(n);
+//
+//        // THEN
+//        Assert.assertEquals(Lists.newArrayList(2, 3), result);
+//
+//    }
+//
+//    @Test
+//    public void should_test8() throws Exception {
+//        // GIVEN
+//        int n = 8;
+//
+//        // WHEN
+//        List<Integer> result = PrimeFactors.generate(n);
+//
+//        // THEN
+//        Assert.assertEquals(Lists.newArrayList(2, 2, 2), result);
+//    }
+//
+//    @Test
+//    public void should_test_9() throws Exception {
+//        // GIVEN
+//        int n = 9;
+//
+//        // WHEN
+//        List<Integer> result = PrimeFactors.generate(n);
+//
+//        // THEN
+//        Assert.assertEquals(Lists.newArrayList(3, 3), result);
+//
+//    }
+}
